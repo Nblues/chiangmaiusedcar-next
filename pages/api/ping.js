4c5ebf9db76f0274b84d3f5305631242001ede13
ChiangMai Dev <dev@chiangmaiusedcar.com>
2025-10-16 01:11:42 +0700

    fix: restore ping API endpoint

---
+++ pages/api/ping.js
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  
+  return res.status(200).send('pong');
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
