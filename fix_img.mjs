ad5413331d00093fdfd9318893da69ac9b3b6e83
ChiangMai Dev <dev@chiangmaiusedcar.com>
2026-04-19 18:14:04 +0700

    fix: remove lighthouse JSON dumps crashing memory limit during build

--- fix_img.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-let code = fs.readFileSync('components/TikTokFeed.jsx', 'utf-8');
-code = code.replace(/\{.*?eslint-disable-next-line[^\n]*\n\s*<img/g, '<img');
-code = code.replace('<img', '{/* eslint-disable-next-line @next/next/no-img-element */}\n                    <img');
-fs.writeFileSync('components/TikTokFeed.jsx', code);
